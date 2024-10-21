98a29f8f4f9f5db27b0af8efab5ca75164c8602d
Eugenii B. Petrov <127658418+EPoY74@users.noreply.github.com>
2024-10-21 20:14:05 +0500

    first commit

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+// https://learn.microsoft.com/ru-ru/training/modules/csharp-literals-variables/8-knowledge-check
+/*
+* Автор: Евгений Петров
+* Почта: p174@mail.ru
+* Цель: Обечение c#
+* Дата проекта: 21.10.2024
+*/
+
+Console.WriteLine(34.40M);
